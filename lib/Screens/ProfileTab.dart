b8a00a263f5fd768ea56800185b784a85b770721
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-03-25 16:31:40 +0530

    CustomClipper were added and profile page is painted with too mush colors .

--- lib/Screens/ProfileTab.dart
+++ lib/Screens/ProfileTab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileTab extends StatefulWidget {
   @override
@@ -8,11 +9,93 @@ class ProfileTab extends StatefulWidget {
 class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Theme.of(context).primaryColor,
-        child: new Icon(Icons.person),
-      ),
+    return new Stack(
+      children: <Widget>[
+        new Container(
+          height: double.infinity,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.grey.shade500,
+        ),
+        new ClipPath(
+          clipBehavior: Clip.antiAlias,
+          clipper: new MyClipper(),
+          child: new Container(
+            height: MediaQuery.of(context).size.height * 0.725,
+            color: Colors.deepOrange,
+            child: new Column(
+              children: <Widget>[
+                new Expanded(
+                  flex: 4,
+                  child: new Container(
+                    color: Colors.deepPurple,
+                    child: new Column(
+                      children: <Widget>[
+                        new SizedBox(height: ScreenUtil().setHeight(50.0)),
+                        new ClipRRect(
+                          borderRadius: BorderRadius.circular(500.0),
+                          child: new Image(
+                            fit: BoxFit.cover,
+                              height: ScreenUtil().setHeight(350.0),
+                              width: ScreenUtil().setWidth(430.0),
+                              image: new AssetImage(
+                                  'assets/images/abhishekProfile.JPG')),
+                        ),
+                        new SizedBox(height : ScreenUtil().setHeight(10.0)),
+                        new Text(
+                          "Abhishek, 19",
+                          style: new TextStyle(
+                            letterSpacing: 1.1,
+                            fontSize: ScreenUtil().setSp(70),
+                            fontWeight: FontWeight.w400
+                          ),
+                        ),
+                        new Expanded(
+                          child: new Row(
+                            children: <Widget>[
+                              new Expanded(
+                                child: Container(color:Colors.red)
+                              ),
+                              new Expanded(
+                                child: Container(color:Colors.amber)
+                              ),
+                              new Expanded(
+                                child: Container(color:Colors.green)
+                              )
+                            ],
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                new Expanded(
+                    flex: 3,
+                    child: new Container(
+                      color: Colors.indigo,
+                    ))
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path p = new Path();
+    p.lineTo(0, size.height - ScreenUtil().setHeight(200));
+    Offset controlPoint = new Offset(size.width / 2, size.height);
+    p.quadraticBezierTo(controlPoint.dx, controlPoint.dy, size.width,
+        size.height - ScreenUtil().setHeight(200));
+    //p.lineTo(size.width,size.height - ScreenUtil().setHeight(200) );
+    p.lineTo(size.width, 0);
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
         secondaryHeaderColor: Color.fromRGBO(255, 88, 100, 1.0),
         accentColor: Color.fromRGBO(255, 101, 91, 1.0)
       ),
-
       home : SplashScreen(),
     );
   }
