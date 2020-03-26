510255e0a0b084e2899513b941042443755a9b72
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-03-26 18:59:36 +0530

    Do you believe in love at first site ? we'll see Tinder Tab

--- lib/Screens/TinderTab.dart
+++ lib/Screens/TinderTab.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TinderTab extends StatefulWidget {
   @override
@@ -12,17 +15,196 @@ class _TinderTabState extends State<TinderTab> {
     return Stack(
       children: <Widget>[
         new Container(
-          color: Colors.white,
+          color: Colors.blue.shade50,
         ),
         new Positioned(
           bottom: 0.0,
           child: new Container(
             height: ScreenUtil().setWidth(220.0),
             width: MediaQuery.of(context).size.width,
-            color: Colors.purpleAccent,
             child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                
+                new Container(
+                  padding: EdgeInsets.all(ScreenUtil().setWidth(15.0)),
+                  height: ScreenUtil().setHeight(80.0),
+                  width: ScreenUtil().setHeight(80.0),
+                  decoration: new BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                            offset: new Offset(0.0, 0.0), color: Colors.grey),
+                        new BoxShadow(
+                            offset: new Offset(1.0, 1.0),
+                            color: Colors.grey,
+                            blurRadius: 5.0),
+                        new BoxShadow(
+                            offset: new Offset(-1.0, -1.0),
+                            color: Colors.white,
+                            blurRadius: 10.0)
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(60.0)),
+                  child: new ShaderMask(
+                      child: new Image(
+                          image: new AssetImage('assets/images/round.png')),
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            colors: [
+                              Colors.amber.shade700,
+                              Colors.amber.shade400
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 1.0]).createShader(bounds);
+                      }),
+                ),
+                new Container(
+                  padding: EdgeInsets.all(ScreenUtil().setSp(30.0)),
+                  height: ScreenUtil().setHeight(110.0),
+                  width: ScreenUtil().setHeight(110.0),
+                  decoration: new BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                            offset: new Offset(0.0, 0.0), color: Colors.grey),
+                        new BoxShadow(
+                            offset: new Offset(1.0, 1.0),
+                            color: Colors.grey,
+                            blurRadius: 5.0),
+                        new BoxShadow(
+                            offset: new Offset(-1.0, -1.0),
+                            color: Colors.white,
+                            blurRadius: 10.0)
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(60.0)),
+                  child: new ShaderMask(
+                      child: new Image(
+                        image: AssetImage('assets/images/closeRounded.png'),
+                      ),
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            colors: [
+                              Theme.of(context).accentColor,
+                              Theme.of(context).primaryColor
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 1.0]).createShader(bounds);
+                      }),
+                ),
+                new Container(
+                  height: ScreenUtil().setHeight(80.0),
+                  width: ScreenUtil().setHeight(80.0),
+                  decoration: new BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                            offset: new Offset(0.0, 0.0), color: Colors.grey),
+                            new BoxShadow(
+                          offset: new Offset(1.0, 1.0),
+                          color: Colors.grey,
+                          blurRadius: 5.0
+                        ),
+                        new BoxShadow(
+                          offset: new Offset(-1.0, -1.0),
+                          color: Colors.white,
+                          blurRadius: 10.0
+                        )
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(60.0)),
+                  child: new ShaderMask(
+                      child: new Icon(
+                        Icons.star,
+                        size: ScreenUtil().setHeight(65.0),
+                      ),
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            colors: [
+                              Colors.blue.shade400,
+                              Colors.blue.shade300
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 1.0]).createShader(bounds);
+                      }),
+                ),
+                new Container(
+                  padding: EdgeInsets.all(ScreenUtil().setSp(30.0)),
+                  height: ScreenUtil().setHeight(110.0),
+                  width: ScreenUtil().setHeight(110.0),
+                  decoration: new BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                            offset: new Offset(0.0, 0.0), color: Colors.grey),
+                            new BoxShadow(
+                          offset: new Offset(1.0, 1.0),
+                          color: Colors.grey,
+                          blurRadius: 5.0
+                        ),
+                        new BoxShadow(
+                          offset: new Offset(-1.0, -1.0),
+                          color: Colors.white,
+                          blurRadius: 10.0
+                        )
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(60.0)),
+                  child: new ShaderMask(
+                      child: new Icon(
+                        Icons.favorite,
+                        size: ScreenUtil().setHeight(65.0),
+                      ),
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            colors: [
+                              Colors.tealAccent.shade700,
+                              Colors.tealAccent.shade200
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 1.0]).createShader(bounds);
+                      }),
+                ),
+                new Container(
+                  padding: EdgeInsets.all(ScreenUtil().setWidth(15.0)),
+                  height: ScreenUtil().setHeight(80.0),
+                  width: ScreenUtil().setHeight(80.0),
+                  decoration: new BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(
+                            offset: new Offset(0.0, 0.0), color: Colors.grey),
+                            new BoxShadow(
+                          offset: new Offset(1.0, 1.0),
+                          color: Colors.grey,
+                          blurRadius: 5.0
+                        ),
+                        new BoxShadow(
+                          offset: new Offset(-1.0, -1.0),
+                          color: Colors.white,
+                          blurRadius: 10.0
+                        )
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(60.0)),
+                  child: new ShaderMask(
+                      child: new Image(
+                          image: new AssetImage('assets/images/lighting.png')),
+                      blendMode: BlendMode.srcATop,
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                            colors: [
+                              Colors.purple.shade500,
+                              Colors.purple.shade200
+                            ],
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 1.0]).createShader(bounds);
+                      }),
+                )
               ],
             ),
           ),
@@ -54,6 +236,12 @@ class _TinderTabState extends State<TinderTab> {
             ),
             height: MediaQuery.of(context).size.height * 0.78,
             width: MediaQuery.of(context).size.width - 10.0,
+            child: new ClipRRect(
+              borderRadius: new BorderRadius.circular(10.0),
+              child: new Image(
+                  fit: BoxFit.cover,
+                  image: new AssetImage("assets/images/person3.jpg")),
+            ),
           ),
           new Positioned(
             bottom: ScreenUtil().setHeight(40.0),
